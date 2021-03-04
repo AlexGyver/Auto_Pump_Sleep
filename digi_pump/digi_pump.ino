9a41117d928d85646d3ddd177b1ee5f263a4f0c3
Alexey Silichenko <a.silichenko@gmail.com>
2021-03-04 16:46:52 +0200

    Update digi_pump.ino
    добавлен код опроса датчика наличия воды

--- digi_pump/digi_pump.ino
+++ digi_pump/digi_pump.ino
@@ -8,6 +8,8 @@
 
 #define PERIOD 259200   // период работы в секундах (пример: 60*60*24*3 = 259200 - три дня!)
 #define WORK 30         // время работы в секундах
+#define USE_SENSOR true // false если без датчика наличия воды
+#define SENSOR 0        // пин датчика наличия воды
 #define MOS 1           // пин мосфета
 
 uint32_t mainTimer, myTimer;
@@ -40,14 +42,14 @@ void loop() {
   mainTimer++;
 
   if (!state) {                           // если помпа не включена
-    if ((long)mainTimer - myTimer > PERIOD) {   // таймер периода
+    if (((long)mainTimer - myTimer > PERIOD) && isWaterPresent()) {   // таймер периода
       myTimer = mainTimer;                // сброс таймера
       state = true;                       // флаг на запуск
       pinMode(MOS, OUTPUT);               // пин как выход
       digitalWrite(MOS, HIGH);            // врубить
     }
   } else {                                // если помпа включена
-    if ((long)mainTimer - myTimer > WORK) {     // таймер времени работы
+    if (((long)mainTimer - myTimer > WORK) || !isWaterPresent()) {     // таймер времени работы
       myTimer = mainTimer;                // сброс
       state = false;                      // флаг на выкл
       digitalWrite(MOS, LOW);             // вырубить
@@ -61,4 +63,23 @@ void loop() {
 
 ISR (WDT_vect) {
   WDTCR |= _BV(WDIE); // разрешаем прерывания по ватчдогу. Иначе будет реcет.
+}
+
+/**
+ * Проверка наличия воды для помпы.
+ *
+ * Бросить в воду два провода: GND и вывод пина SENSOR.
+ * Вода должна замыкать эти провода,
+ * в таком случае помпу можно включать.
+ * 
+ * Если воды нет - пин SENSOR не подключен к GND,
+ * на нем высокий уровень сигнала,
+ * значит помпу выключить и не включать.
+ */
+void isWaterPresent() {
+  if (!USE_SENSOR) return true; // без датчика ничего не проверяем, просто качаем
+  pinMode(SENSOR, INPUT_PULLUP); // чтобы когда не будет воды, вывод не ловил наводки из эфира
+  boolean retval = LOW == digitalRead(SENSOR); // если низкий уровень сигнала - значит есть контакт с землей, а значит есть вода
+  pinMode(SENSOR, INPUT); // снимаем подтяжку к VCC для экономии энергии
+  return retval;
 }
